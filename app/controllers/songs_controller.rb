c90fbcc1e061bfd57e921632586217921eb94986
Peter-G-Stone <peter.stone.online@gmail.com>
2018-12-04 23:58:56 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,6 +13,8 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
+    
+    params[:song_notes].each {|n| @song.notes << Note.new({content: n})}
 
     if @song.save
       redirect_to @song
@@ -47,7 +49,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :song_notes => [])
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,49 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  before_save 
+
+  
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+     self.artist ? self.artist.name : nil
+  end
+
+  def genre_id=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_id
+     self.genre ? self.genre.name : nil
+  end
+
+  def note_contents=(notes)
+    notes.each {|n| self.notes << Note.new(content: n) if n != ""}
+  end
+
+  def note_contents
+    self.notes.collect { |n| n.content }
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+     self.genre ? self.genre.name : nil
+  end
+
+  # def song_notes=(notes)
+  #   notes.each {|n| self.notes << n}
+  # end
+
+  # def song_notes
+  #   self.notes
+  # end
 end
